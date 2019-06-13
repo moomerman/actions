b38efa9dfa013cdc570bcd0a97a17e4fbb56bcdc
Richard Taylor <moomerman@gmail.com>
2019-06-13 22:09:11 +0100

    Archive one level higher

--- bin/ghr/entrypoint.sh
+++ bin/ghr/entrypoint.sh
@@ -6,13 +6,13 @@ gh_repo=`echo $GITHUB_REPOSITORY | sed -e 's/.*\///g'`
 
 revision=`git describe`
 version=`echo $revision | cut -d "-" -f 1`.`echo $revision | cut -d "v" -f 2 | cut -d "-" -f 2`
-release="_build/$MIX_ENV/rel/$APPLICATION"
+release="$RELEASE_PATH/$APPLICATION"
 archive="$APPLICATION-`echo $version | cut -d "v" -f 2`.tar.gz"
 
 echo $revision > $release/REVISION
 echo $version > $release/VERSION
 
-tar -zcvf $archive -C $release .
+tar -zcvf $archive -C $RELEASE_PATH .
 
 ghr -v
 sh -c "ghr -t $GITHUB_TOKEN -u $gh_user -r $gh_repo -n $version $version $archive"
