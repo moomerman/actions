dbd3a26f34c3f347dbf3e88ffb64ef57180c3c09
Richard Taylor <moomerman@gmail.com>
2019-06-13 20:56:10 +0100

    Move more work into the ghr entrypoint

--- bin/ghr/entrypoint.sh
+++ bin/ghr/entrypoint.sh
@@ -3,7 +3,14 @@ set -ex
 
 gh_user=`echo $GITHUB_REPOSITORY | sed -e 's/\/.*//g'`
 gh_repo=`echo $GITHUB_REPOSITORY | sed -e 's/.*\///g'`
-version=`echo $RELEASE/VERSION`
+
+revision=`git describe --dirty --always --tags --first-parent --abbrev=1`
+version=`echo $revision | cut -d "-" -f 1`.`git describe | cut -d "v" -f 2 | cut -d "-" -f 2`
+
+echo $revision > $RELEASE/REVISION
+echo $version > $RELEASE/VERSION
+
+tar -zcvf $version.tar.gz $RELEASE
 
 ghr -v
-sh -c "ghr -t $GITHUB_TOKEN -u $gh_user -r $gh_repo -n $version $version $*"
+sh -c "ghr -t $GITHUB_TOKEN -u $gh_user -r $gh_repo -n $version $version $version.tar.gz"
