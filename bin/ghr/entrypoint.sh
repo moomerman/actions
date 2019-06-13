161fe726d27edaab70b43f577c69a64ef7a8ad8f
Richard Taylor <moomerman@gmail.com>
2019-06-13 19:34:11 +0100

    Put more of the work in the entrypoint

--- bin/ghr/entrypoint.sh
+++ bin/ghr/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -ex
 
+gh_user=`echo $GITHUB_REPOSITORY | sed -e 's/\/.*//g'`
+gh_repo=`echo $GITHUB_REPOSITORY | sed -e 's/.*\///g'`
+version=`echo $RELEASE/VERSION`
+
 ghr -v
-sh -c "ghr $*"
+sh -c "ghr -t $GITHUB_TOKEN -u $gh_user -r $gh_repo -n $version $version $*"
