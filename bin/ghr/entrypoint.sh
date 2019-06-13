d312770e1f4276f368ee18db2690cb33808b4a7a
Richard Taylor <moomerman@gmail.com>
2019-06-13 21:16:36 +0100

    Clean up the ghr release

--- bin/ghr/entrypoint.sh
+++ bin/ghr/entrypoint.sh
@@ -4,13 +4,15 @@ set -ex
 gh_user=`echo $GITHUB_REPOSITORY | sed -e 's/\/.*//g'`
 gh_repo=`echo $GITHUB_REPOSITORY | sed -e 's/.*\///g'`
 
-revision=`git describe --dirty --always --tags --first-parent --abbrev=1`
-version=`echo $revision | cut -d "-" -f 1`.`git describe | cut -d "v" -f 2 | cut -d "-" -f 2`
+revision=`git describe`
+version=`echo $revision | cut -d "-" -f 1`.`echo $revision | cut -d "v" -f 2 | cut -d "-" -f 2`
+release="_build/$MIX_ENV/rel/$APPLICATION"
+archive="$APPLICATION-`cat $version | cut -d "v" -f 2`.tar.gz"
 
 echo $revision > $RELEASE/REVISION
 echo $version > $RELEASE/VERSION
 
-tar -zcvf $version.tar.gz $RELEASE
+tar -zcvjf $archive $release
 
 ghr -v
-sh -c "ghr -t $GITHUB_TOKEN -u $gh_user -r $gh_repo -n $version $version $version.tar.gz"
+sh -c "ghr -t $GITHUB_TOKEN -u $gh_user -r $gh_repo -n $version $version $archive"
