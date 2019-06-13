4c7ea2b652673f26196f5e99d80675d15395065a
Richard Taylor <moomerman@gmail.com>
2019-06-13 21:36:11 +0100

    Fix version, change dir for tar

--- bin/ghr/entrypoint.sh
+++ bin/ghr/entrypoint.sh
@@ -7,12 +7,12 @@ gh_repo=`echo $GITHUB_REPOSITORY | sed -e 's/.*\///g'`
 revision=`git describe`
 version=`echo $revision | cut -d "-" -f 1`.`echo $revision | cut -d "v" -f 2 | cut -d "-" -f 2`
 release="_build/$MIX_ENV/rel/$APPLICATION"
-archive="$APPLICATION-`cat $version | cut -d "v" -f 2`.tar.gz"
+archive="$APPLICATION-`echo $version | cut -d "v" -f 2`.tar.gz"
 
 echo $revision > $release/REVISION
 echo $version > $release/VERSION
 
-tar -zcvjf $archive $release
+tar -zcvf $archive -C $release
 
 ghr -v
 sh -c "ghr -t $GITHUB_TOKEN -u $gh_user -r $gh_repo -n $version $version $archive"
